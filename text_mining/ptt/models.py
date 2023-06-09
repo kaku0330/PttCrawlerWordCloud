3b8d6415d48dd4f820b4468321abe9c8fa3070af
johnny <tmpss94182@gmail.com>
2023-06-09 15:02:09 +0800

    copy file from gitlab to github

---
+++ text_mining/ptt/models.py
@@ -0,0 +1,20 @@
+from __future__ import unicode_literals
+
+from django.db import models
+import uuid, json
+from datetime import datetime, timedelta
+import time, json
+
+# Create your models here.
+
+
+class PTT(models.Model):
+    article_id = models.CharField(db_column = 'article_id', unique = True, max_length = 64)
+    author = models.CharField(db_column = 'author', unique = False, max_length = 128, null = True)
+    title = models.CharField(db_column = 'title', unique = False, max_length = 256, null = True)
+    content = models.CharField(db_column = 'content', unique = False, max_length = 4096, null = True)
+    date = models.DateTimeField(db_column = 'date', null = True)
+    ip = models.CharField(db_column = 'ip', unique = False, max_length = 64, null = True)
+
+    class Meta:
+        db_table = 'PTT'
---
+++ text_mining/ptt/urls.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from django.urls import path
+from django.conf.urls import url
+
+from ptt import views as ptt
+
+urlpatterns = [
+    # path('home', views.home),
+    # path('getdata', views.getdata),
+
+    url(r'^$', ptt.index),
+    url(r'^getdata/$', ptt.getdata),
+    url(r'^crawler/$',ptt.crawler),
+    url(r'^test/$',ptt.test),
+	
+]
